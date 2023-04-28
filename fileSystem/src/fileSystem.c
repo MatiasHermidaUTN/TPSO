6ffc4adb2ce15cdc7e7d7f8f5acd61f28d4a10a1
MattP2001 <mpereda@frba.utn.edu.ar>
2023-04-27 23:19:52 -0300

    Escribir y deprecado de funciones no utiles

--- fileSystem/src/fileSystem.c
+++ fileSystem/src/fileSystem.c
@@ -62,14 +62,14 @@ int main(int argc, char** argv) {
 		bloques = fopen(lectura_de_config.PATH_BLOQUES, "w+");
 	}
 
-	limpiar_bitmap();
+	//limpiar_bitmap();
 	printf("unos inicial: %d\n", cant_unos_en_bitmap());
 
 	//SE CONECTA AL SERIVDOR MEMORIA
-/*
+
 	int socket_memoria = crear_conexion(lectura_de_config.IP_MEMORIA, lectura_de_config.PUERTO_MEMORIA);
     enviar_handshake(socket_memoria, FILESYSTEM);
-*/
+
 	//SE HACE SERVIDOR Y ESPERA LA CONEXION DEL KERNEL
 	//TODO CREAR UN HILO PARA ESPERAR EL CLIENTE(KERNEL)
 
@@ -137,7 +137,7 @@ int main(int argc, char** argv) {
 				printf("apartir_de_donde_X: %d\n", apartir_de_donde_X);
 				printf("cuanto_X: %d\n", cuanto_X);
 				printf("dir_fisica_memoria: %d\n", dir_fisica_memoria);
-				//buffer = leer_archivo(nombre_archivo, apartir_de_donde_X, cuanto_X);	//malloc se hace en leer_archivo
+				buffer = leer_archivo(nombre_archivo, apartir_de_donde_X, cuanto_X);	//malloc se hace en leer_archivo
 				//mandar_a_memoria(socket_memoria, ESCRIBIR, buffer, cuanto_X, dir_fisica_memoria);
 				//enviar_mensaje_kernel(kernel, "OK Archivo leido");
 				//free(buffer);
@@ -151,7 +151,7 @@ int main(int argc, char** argv) {
 				printf("cuanto_X: %d\n", cuanto_X);
 				printf("dir_fisica_memoria: %d\n", dir_fisica_memoria);
 				//buffer = leer_de_memoria(socket_memoria, LEER, cuanto_X, dir_fisica_memoria);	//malloc se hace en leer_de_memoria
-				//escribir_archivo(buffer, nombre_archivo, apartir_de_donde_X, cuanto_X);
+				escribir_archivo(buffer, nombre_archivo, apartir_de_donde_X, cuanto_X);
 				//enviar_mensaje_kernel(kernel, "OK Archivo escrito");
 				//free(buffer);
 				free(nombre_archivo);
@@ -170,6 +170,82 @@ int main(int argc, char** argv) {
 	munmap(bitmap_pointer, tamanioBitmap);
 	fclose(bloques);
 	return EXIT_SUCCESS;
+}
+
+void escribir_archivo(char* buffer, char* nombre_archivo, int apartir_de_donde_escribir, int cuanto_escribir) {	//llega del switch
+	if(!hay_espacio_suficiente(cuanto_escribir)){ //chequea que pueda escribi todo!
+		log_error(logger, "No hay espacio suficiente para escribir");
+		exit(EXIT_FAILURE);
+	}
+	if (apartir_de_donde_escribir > tamanio_archivo) {		//failsafe - no escribir donde no hay
+		return "ERROR, apartir_de_donde_leer es > al tamanio_archivo";
+	}
+	int cantidad_escrita = 0;
+
+	char* path = obtener_path_FCB_sin_free(nombre_archivo);
+	t_config* archivo_FCB = iniciar_config(path);
+	int tamanio_archivo = config_get_int_value(archivo_FCB, "TAMANIO_ARCHIVO");
+	free(path);
+
+	if(apartir_de_donde_escribir + cuanto_escribir > tamanio_archivo){
+		int nuevo_tamanio_archivo = apartir_de_donde_escribir + cuanto_escribir;
+
+		truncar_archivo(nombre_archivo, nuevo_tamanio_archivo);
+	}
+
+	if (apartir_de_donde_escribir < super_bloque_info.block_size){
+		uint32_t puntero_directo = config_get_int_value(archivo_FCB, "PUNTERO_DIRECTO");
+		fseek(bloques, puntero_directo * super_bloque_info.block_size, SEEK_SET);
+		int dist_fondo_bloque_apartir_de_donde_esc = super_bloque_info.block_size - apartir_de_donde_escribir;
+
+		if(cuanto_escribir < dist_fondo_bloque_apartir_de_donde_esc){
+			escribir_bloque(buffer, puntero_directo, apartir_de_donde_escribir, &cuanto_escribir, &cantidad_escrita);
+		}else{
+			escribir_bloque(buffer, puntero_directo, apartir_de_donde_escribir, &cuanto_escribir, &cantidad_escrita);				
+			sobreescribir_indirecto(&buffer, archivo_FCB, PRIMER_BLOQUE_SECUNDARIO, &cuanto_escribir, &cantidad_escrita);
+		}
+	}
+	else {
+		int bloque_secundario_inicial = (int) ceil((float) apartir_de_donde_escribir / super_bloque_info.block_size - 1);
+		int apartir_de_donde_escribir_relativo_a_bloque = apartir_de_donde_escribir % super_bloque_info.block_size;
+		escribir_bloque(buffer, bloque_secundario_inicial, apartir_de_donde_escribir_relativo_a_bloque, &cuanto_escribir, &cantidad_escrita);
+		sobreescribir_indirecto(&buffer, archivo_FCB, bloque_secundario_inicial + 1, &cuanto_escribir, &cantidad_escrita);
+	}
+	
+	config_destroy(archivo_FCB);
+	return;
+}
+
+void sobreescribir_indirecto(char* buffer, t_config* archivo_FCB, int bloque_secundario_donde_escribir, int* cuanto_escribir, int* cantidad_escrita) {
+	int tamanio_archivo = config_get_int_value(archivo_FCB, "TAMANIO_ARCHIVO");
+	uint32_t puntero_indirecto = config_get_int_value(archivo_FCB, "PUNTERO_INDIRECTO");
+	uint32_t puntero_secundario;			
+
+	while(cuanto_escribir < 0) {
+		fseek(bloques, puntero_indirecto * super_bloque_info.block_size + sizeof(puntero_secundario) * bloque_secundario_donde_escribir, SEEK_SET);
+		fread(&puntero_secundario, sizeof(puntero_secundario), 1, bloques);	
+		escribir_bloque(buffer, puntero_secundario, 0, &cuanto_escribir, &cantidad_escrita, super_bloque_info.block_size);
+		bloque_secundario_donde_escribir++;
+	}
+}
+
+void escribir_bloque(char* buffer, uint32_t puntero, int apartir_de_donde_escribir_relativo_a_puntero, int* cuanto_escribir, int* cantidad_escrita) {
+	fseek(bloques, puntero * super_bloque_info.block_size + apartir_de_donde_escribir_relativo_a_puntero, SEEK_SET);
+
+	char* copia_buffer = (char*) string_substring(buffer, *cantidad_escrita, *cuanto_escribir);
+
+	if(apartir_de_donde_escribir_relativo_a_puntero + *cuanto_escribir > super_bloque_info.block_size){
+		int dist_fondo_bloque_apartir_de_donde_esc = super_bloque_info.block_size - apartir_de_donde_escribir_relativo_a_bloque;
+		fwrite(copia_buffer, dist_fondo_bloque_apartir_de_donde_esc, 1, bloques);
+
+		*cantidad_escrita += dist_fondo_bloque_apartir_de_donde_esc;
+		*cuanto_escribir -= dist_fondo_bloque_apartir_de_donde_esc;
+	}
+	else {
+		fwrite(copia_buffer, *cuanto_escribir, 1, bloques);
+		*cantidad_escrita += *cuanto_escribir;
+		*cuanto_escribir = 0;
+	}
 }
 
 void truncar(char* nombre_archivo, int nuevo_tamanio_archivo){
@@ -293,13 +369,6 @@ char* obtener_path_FCB_sin_free(char* nombre_archivo){
 	strcat(path, nombre_archivo);		//se asume que en la FS.config el path_fcb tiene / al final quedando: "/home/utnso/fs/fcb/"
 	return path;
 }
-	
-/*void delete_FCB(char* nombre_archivo) {
-	char* path;
-	strcpy(path, lectura_de_config.PATH_FCB);
-	strcat(path, nombre_archivo);		//se asume que en la FS.config el path_fcb tiene / al final quedando: "/home/utnso/fs/fcb/"
-	remove(path);
-}*/ //hace falta esto???
 
 char* leer_archivo(char* nombre_archivo, int apartir_de_donde_leer, int cuanto_leer) {
 	char* path = obtener_path_FCB_sin_free(nombre_archivo);
@@ -376,78 +445,6 @@ void leer_indirecto(char** buffer, t_config* archivo_FCB, int bloque_secundario_
 	}
 }
 
-void escribir_archivo(char* buffer, char* nombre_archivo, int apartir_de_donde_escribir, int cuanto_escribir) {
-	char* cuanto_escribir_string, puntero_directo_string, puntero_indirecto_string;
-	int cantidad_escrita = 0;
-
-
-	char* path = obtener_path_FCB_sin_free(nombre_archivo);
-	t_config* archivo_FCB = iniciar_config(path);
-	free(path);
-	
-	int tamanio_archivo = config_get_int_value(archivo_FCB, "TAMANIO_ARCHIVO");
-
-	if(checkear_espacio(cuanto_escribir))//chequea que pueda escribi todo!!!!
-	{
-		if (tamanio_archivo == 0){		//solo para archivos NUEVOS
-			cuanto_escribir_string = string_itoa(cuanto_escribir);
-			config_set_value(archivo_FCB, "TAMANIO_ARCHIVO", cuanto_escribir_string);
-
-			uint32_t puntero_directo = dame_un_bloque_libre();
-			puntero_directo_string = string_itoa(puntero_directo);
-			config_set_value(archivo_FCB, "PUNTERO_DIRECTO", puntero_directo_string);
-			escribir_bloque(&buffer, puntero_directo, &cuanto_escribir, &cantidad_escrita, super_bloque_info.block_size);
-			
-			if(cuanto_escribir > 0){
-				uint32_t puntero_indirecto = dame_un_bloque_libre();
-				puntero_indirecto_string = string_itoa(puntero_indirecto);
-				config_set_value(archivo_FCB, "PUNTERO_INDIRECTO", puntero_indirecto_string);
-
-				for(int cant_punteros_secundarios = 0 ; cuanto_escribir > 0 ; cant_punteros_secundarios++) {
-					uint32_t puntero = dame_un_bloque_libre();
-					fseek(bloques, puntero_indirecto * super_bloque_info.block_size + sizeof(puntero) * cant_punteros_secundarios, SEEK_SET);
-					fwrite(puntero, sizeof(puntero), 1, bloques);				//anoto nuevo puntero_secundario en bloque de puntero_indirecto
-					escribir_bloque(&buffer, puntero, &cuanto_escribir, &cantidad_escrita, super_bloque_info.block_size);
-				}
-			}
-		}
-		else {
-			uint32_t puntero_directo = config_get_uint_value(archivo_FCB, "PUNTERO_DIRECTO");
-			uint32_t puntero_indirecto = config_get_uint_value(archivo_FCB, "PUNTERO_INDIRECTO");
-
-			cuanto_escribir_string = intToCharAsterisco((unsigned int) (cuanto_escribir + tamanio_archivo));
-			config_set_value(archivo_FCB, "TAMANIO_ARCHIVO", cuanto_escribir_string);
-
-			int cantidad_de_bloques_ocupados = (tamanio_archivo / super_bloque_info.block_size);	//ej: 120 / 64 = 1 (trunca)
-			if(cantidad_de_bloques_ocupados == 0) {
-				escribir_bloque(&buffer, puntero_directo, &cuanto_escribir, &cantidad_escrita, super_bloque_info.block_size);
-			}
-			else {
-				uint32_t puntero;
-				cantidad_de_bloques_ocupados--;	//resto el bloque correspondiente al puntero directo
-				fseek(bloques, puntero_indirecto * super_bloque_info.block_size + sizeof(puntero) * cantidad_de_bloques_ocupados, SEEK_SET);
-				fread(&puntero, sizeof(puntero), 1, bloques);
-				
-				int espacio_libre_en_bloque = tamanio_archivo - cantidad_de_bloques_ocupados * super_bloque_info.block_size;
-				escribir_bloque(&buffer, puntero, &cuanto_escribir, &cantidad_escrita, espacio_libre_en_bloque);
-				cantidad_de_bloques_ocupados++;
-
-				for(int cant_punteros_secundarios = cantidad_de_bloques_ocupados ; cuanto_escribir > 0 ; cant_punteros_secundarios++) {
-					uint32_t puntero = dame_un_bloque_libre();
-					fseek(bloques, puntero_indirecto * super_bloque_info.block_size + sizeof(puntero) * cant_punteros_secundarios, SEEK_SET);
-					fwrite(puntero, sizeof(puntero), 1, bloques);				//anoto nuevo puntero_secundario en bloque de puntero_indirecto
-					escribir_bloque(&buffer, puntero, &cuanto_escribir, &cantidad_escrita, super_bloque_info.block_size);
-				}
-			}
-		}
-	}
-	
-	free(cuanto_escribir_string);
-	free(puntero_directo_string);
-	free(puntero_indirecto_string);
-	config_destroy(archivo_FCB);
-}
-
 bool archivo_se_puede_leer(char* path)
 {
 	FILE* f;
@@ -457,59 +454,6 @@ bool archivo_se_puede_leer(char* path)
 	} else {
 		return 0;
 	}
-	//return (!access(path, R_OK ));
-}
-
-bool archivo_se_puede_escribir(char* path)
-{
-	return (!access(path, W_OK ));
-}
-
-//Prueba int to char*
-// seguramente haya q pasar por referencia el char*
-char* intToCharAsterisco(unsigned int numero) {
-
-	numero = abs(numero);
-
-	int digitos = cantidad_de_digitos(numero);
-
-	char* string = malloc(digitos + 1);
-	int digito;
-
-	if(numero == 0)
-		return "0";
-
-	for (int i = 0, j = digitos - 1, copiaNumero = numero; numero > 0; i++, j--) {
-        copiaNumero /= 10;
-        digito = numero - (copiaNumero * 10);
-        string[j] = digito  + '0';
-        numero = copiaNumero;
-    }
-
-    return string;
-}
-
-int cantidad_de_digitos(unsigned int numero) { 
-  int cociente = 0, divisor = 1, contador = 0;
-
-  while (cociente !=1){
-    contador++;
-    divisor = divisor * 10;
-    cociente = numero / divisor;
-
-    if (cociente == 1){
-    	return contador + 1;
-    }
-
-    if (cociente == 0){
-    	return contador;
-    }
-
-    if (numero < 10){
-    	return 1;
-    }
-  }
-  return 0;
 }
 
 bool checkear_espacio(int cuanto_escribir) {
@@ -553,38 +497,6 @@ void limpiar_bitmap() {
 void liberar_bloque(uint32_t puntero){
 	bitarray_clean_bit(bitarray_de_bitmap, puntero);
 	msync(bitmap_pointer, tamanioBitmap, MS_SYNC);
-}
-
-void escribir_bloque(char* buffer, uint32_t puntero, int* cuanto_escribir, int* cantidad_escrita, int espacio_libre_en_bloque) {
-	int a_partir_de_donde_escribir = super_bloque_info.block_size - espacio_libre_en_bloque;
-	fseek(bloques, puntero * super_bloque_info.block_size + a_partir_de_donde_escribir, SEEK_SET);
-
-	char* copia_buffer = (char*) string_substring(buffer, (int)*cantidad_escrita, (int)*cuanto_escribir);
-
-	if(*cuanto_escribir < espacio_libre_en_bloque){
-		fwrite(copia_buffer, *cuanto_escribir, 1, bloques);
-		*cantidad_escrita += *cuanto_escribir;
-		*cuanto_escribir = 0;
-	}
-	else {
-		fwrite(copia_buffer, espacio_libre_en_bloque, 1, bloques);
-		*cantidad_escrita += espacio_libre_en_bloque;
-		*cuanto_escribir -= espacio_libre_en_bloque;
-	}
-	free(copia_buffer);
-}
-
-int espacio_libre_en_bloque(uint32_t puntero) {
-	int espacio_libre = 0;
-	fseek(bloques, puntero * super_bloque_info.block_size, SEEK_SET);
-	char* buffer = malloc(super_bloque_info.block_size);
-	fread(buffer, super_bloque_info.block_size, 1, bloques);
-	for (int i = 0; i < super_bloque_info.block_size; i++) {
-		if (buffer[i] == '\0')
-			espacio_libre++;
-	}
-	free(buffer);
-	return espacio_libre;
 }
 
 t_instrucciones recibir_cod_op(int socket_cliente)
