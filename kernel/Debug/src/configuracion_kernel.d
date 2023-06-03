64a3f97e51f964fd3a05a0032203f079e987a964
Agustin Oliva <aoliva@frba.utn.edu.ar>
2023-06-03 12:14:22 +0000

    configs

--- kernel/Debug/src/configuracion_kernel.d
+++ kernel/Debug/src/configuracion_kernel.d
@@ -1,13 +1,13 @@
 src/configuracion_kernel.o: ../src/configuracion_kernel.c \
  ../src/../include/configuracion_kernel.h \
- /home/utnso/tp-2023-1c-Los-Matias/shared/src/utils.h \
- /home/utnso/tp-2023-1c-Los-Matias/shared/src/diccionario_instrucciones.h \
- /home/utnso/tp-2023-1c-Los-Matias/shared/src/utils.h \
+ /home/utnso/Documents/tp-2023-1c-Los-Matias/shared/src/utils.h \
+ /home/utnso/Documents/tp-2023-1c-Los-Matias/shared/src/diccionario_instrucciones.h \
+ /home/utnso/Documents/tp-2023-1c-Los-Matias/shared/src/utils.h \
  ../src/../include/planificacion_largo.h \
  ../src/../include/configuracion_kernel.h
 ../src/../include/configuracion_kernel.h:
-/home/utnso/tp-2023-1c-Los-Matias/shared/src/utils.h:
-/home/utnso/tp-2023-1c-Los-Matias/shared/src/diccionario_instrucciones.h:
-/home/utnso/tp-2023-1c-Los-Matias/shared/src/utils.h:
+/home/utnso/Documents/tp-2023-1c-Los-Matias/shared/src/utils.h:
+/home/utnso/Documents/tp-2023-1c-Los-Matias/shared/src/diccionario_instrucciones.h:
+/home/utnso/Documents/tp-2023-1c-Los-Matias/shared/src/utils.h:
 ../src/../include/planificacion_largo.h:
 ../src/../include/configuracion_kernel.h:
