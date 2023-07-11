8a3cb93df534aec0745416a7b3da6214064142a0
Matias Oliva <matoliva@frba.utn.edu.ar>
2023-07-11 19:09:03 +0000

    cambio get_int a get_string

--- cpu/src/configuracion_cpu.c
+++ cpu/src/configuracion_cpu.c
@@ -17,7 +17,7 @@ t_cpu_config leer_cpu_config(t_config* config) {
     lectura_de_config.RETARDO_INSTRUCCION = config_get_int_value(config, "RETARDO_INSTRUCCION");
     lectura_de_config.PUERTO_ESCUCHA      = config_get_string_value(config, "PUERTO_ESCUCHA");
     lectura_de_config.TAM_MAX_SEGMENTO    = config_get_int_value(config, "TAM_MAX_SEGMENTO");
-    lectura_de_config.IP_CPU  			  = config_get_int_value(config, "IP_CPU");
+    lectura_de_config.IP_CPU  			  = config_get_string_value(config, "IP_CPU");
 
     return lectura_de_config;
 }
