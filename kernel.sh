81d04c8eb4f21b26f0335304cecdf4c183564eb0
Joaquín Altalef <jaltalef@frba.utn.edu.ar>
2023-07-13 23:29:28 +0000

    Saco  de los scripts

--- kernel.sh
+++ kernel.sh
@@ -1,3 +1,3 @@
 cd kernel/Debug
-LD_LIBRARY_PATH=/home/utnso/tp-2023-1c-Los-Matias/shared/Debug $@ ./kernel ../kernel_$1.config
+LD_LIBRARY_PATH=/home/utnso/tp-2023-1c-Los-Matias/shared/Debug ./kernel ../kernel_$1.config
 cd ../../
