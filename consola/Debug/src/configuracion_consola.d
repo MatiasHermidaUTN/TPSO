7b15a578c80e6834834961388a5f7d72cd102218
Joaquín Altalef <jaltalef@frba.utn.edu.ar>
2023-07-13 23:19:46 +0000

    Agrego *.o y *.d a los .gitignore

--- consola/Debug/src/configuracion_consola.d
+++
@@ -1,3 +0,0 @@
-src/configuracion_consola.o: ../src/configuracion_consola.c \
- ../src/../include/configuracion_consola.h
-../src/../include/configuracion_consola.h:
--- shared/Debug/src/diccionario_instrucciones.d
+++
@@ -1,4 +0,0 @@
-src/diccionario_instrucciones.o: ../src/diccionario_instrucciones.c \
- ../src/diccionario_instrucciones.h ../src/utils.h
-../src/diccionario_instrucciones.h:
-../src/utils.h:
