20f7e76630c8b2e931f16f02d7e65e29c038ef14
JoacoAltalef <102761191+JoacoAltalef@users.noreply.github.com>
2023-07-15 10:56:35 -0300

    Ahora set_ips.sh también cambia los de consola

--- set_ips.sh
+++ set_ips.sh
@@ -13,7 +13,7 @@ ip_fs="$3"
 ip_kernel="$4"
 
 # Array con los nombres de directorio
-modulos=("kernel" "cpu" "fileSystem" "memoria")
+modulos=("kernel" "cpu" "fileSystem" "memoria" "consola")
 
 # Array con los nombres de los archivos de configuración
 configs=("base" "deadlock" "error" "memoria" "fs")
@@ -22,30 +22,34 @@ configs=("base" "deadlock" "error" "memoria" "fs")
 for mod in "${modulos[@]}"; do
   config_mod="$mod/configs"
 
-  # Recorro los archivos de configuracion en el directorio
-  for config in "${configs[@]}"; do
-    config_file="$config_mod/${mod}_$config.config"
+  if [ "$mod" == "consola" ]; then
+    sed -i "s/IP_KERNEL=.*/IP_KERNEL="$ip_kernel"/" "$mod/consola.config"
+  else
+    # Recorro los archivos de configuracion en el directorio
+    for config in "${configs[@]}"; do
+      config_file="$config_mod/${mod}_$config.config"
 
-    # Actualizo las lineas en el archivo de configuracion
-    case $mod in
-      "kernel")
-        sed -i "s/IP_KERNEL=.*/IP_KERNEL="$ip_kernel"/; s/IP_MEMORIA=.*/IP_MEMORIA="$ip_memoria"/; s/IP_FILESYSTEM=.*/IP_FILESYSTEM="$ip_fs"/; s/IP_CPU=.*/IP_CPU="$ip_cpu"/" "$config_file"
-        ;;
-      "cpu")
-        sed -i "s/IP_CPU=.*/IP_CPU="$ip_cpu"/; s/IP_MEMORIA=.*/IP_MEMORIA="$ip_memoria"/" "$config_file"
-        ;;
-      "fileSystem")
-        sed -i "s/IP_FILESYSTEM=.*/IP_FILESYSTEM="$ip_fs"/; s/IP_MEMORIA=.*/IP_MEMORIA="$ip_memoria"/" "$config_file"
-        ;;
-      "memoria")
-        sed -i "s/IP_MEMORIA=.*/IP_MEMORIA="$ip_memoria"/" "$config_file"
-        ;;
-      *)
-        echo "Modulo no reconocido: $mod"
-        ;;
-    esac
+      # Actualizo las lineas en el archivo de configuracion
+      case $mod in
+        "kernel")
+          sed -i "s/IP_KERNEL=.*/IP_KERNEL="$ip_kernel"/; s/IP_MEMORIA=.*/IP_MEMORIA="$ip_memoria"/; s/IP_FILESYSTEM=.*/IP_FILESYSTEM="$ip_fs"/; s/IP_CPU=.*/IP_CPU="$ip_cpu"/" "$config_file"
+          ;;
+        "cpu")
+          sed -i "s/IP_CPU=.*/IP_CPU="$ip_cpu"/; s/IP_MEMORIA=.*/IP_MEMORIA="$ip_memoria"/" "$config_file"
+          ;;
+        "fileSystem")
+          sed -i "s/IP_FILESYSTEM=.*/IP_FILESYSTEM="$ip_fs"/; s/IP_MEMORIA=.*/IP_MEMORIA="$ip_memoria"/" "$config_file"
+          ;;
+        "memoria")
+          sed -i "s/IP_MEMORIA=.*/IP_MEMORIA="$ip_memoria"/" "$config_file"
+          ;;
+        *)
+          echo "Modulo no reconocido: $mod"
+          ;;
+      esac
 
-    echo "IP actualizada en $config_file"
-  done
+      echo "IP actualizada en $config_file"
+    done
+  fi
   echo "Configs de $mod actualizados."
 done
